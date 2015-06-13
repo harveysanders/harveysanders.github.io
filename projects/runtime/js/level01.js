5d33d1ee463fbbfab83a2ad853b7831cf3922cb2
Harvey Sanders <sanders.harvey@gmail.com>
2015-06-13 10:54:39 -0500

    Add enemies generator

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -17,13 +17,13 @@
             speed: -3,
             gameItems: [
                 {type: 'sawblade',x:800,y:30},
-                //{type: 'sawblade',x:900,y:20},
-                //{type: 'sawblade',x:1100,y:110},
-                //{type: 'sawblade',x:1900,y:20},
+                {type: 'enemy',x:900,y:20},
+                {type: 'enemy',x:1400,y:30},
+                {type: 'sawblade',x:1900,y:20},
                 {type: 'sawblade',x:1900,y:20},
                 {type: 'sawblade',x:2900,y:320},
                 {type: 'sawblade',x:3200,y:120}
-            ]
+                ]
         };
 
         // set this to true or false depending on if you want to see hitzones
@@ -47,27 +47,37 @@
             game.addGameItem(myObstacle);
         }
         
-        var createScaryHead = function(x,y) {
-            var hitZoneSize = 25;
-            var damageFromObstacle = 40;
-            var myObstacle = game.createObstacle(hitZoneSize, damageFromObstacle);
+        var createEnemy = function(x,y) {
+            var enemy = game.createGameItem('enemy', 25);
             var scaryhead = draw.bitmap('img/scaryhead_small.png');
-            scaryhead.x = -26;
-            scaryhead.y = -30;
-            myObstacle.x = x;
-            myObstacle.y = groundY-y;
-            //myObstacle.rotationalVelocity = 10;
-            myObstacle.addChild(scaryhead);
-            game.addGameItem(myObstacle);
-        }
-        
+            scaryhead.x = -29
+            scaryhead.y = -29;
+            enemy.addChild(scaryhead);
+            enemy.x = x;
+            enemy.y = groundY-y;
+            game.addGameItem(enemy);
+            enemy.velocityX = -1;
+            enemy.rotationalVelocity = 1;
+
+            enemy.onPlayerCollision = function() {
+                console.log('the enemy has hit Halle')
+                game.changeIntegrity(-10);
+            };
+
+            enemy.onProjectileCollision = function() {
+                console.log('halle shot enemy.');
+                game.increaseScore(100);
+                enemy.fadeOut();
+            };
+        };
+
         var createGameItem = function(gameItem){
             switch(gameItem.type) {
-                case "scaryhead":
-                    createScaryHead(gameItem.x, gameItem.y);
-                    break;
-                case "sawblade":
+                case 'sawblade':
                     createSawBlade(gameItem.x, gameItem.y);
+                    break;
+                case 'enemy':
+                    createEnemy(gameItem.x, gameItem.y);
                     break;
                 default:
                     console.log(gameItem.type);
@@ -75,23 +85,10 @@
                     break;
             }
         }
-       
-        //levelData.gameItems.forEach(createGameItem);
-
-        var enemy = game.createGameItem('enemy', 25);
-        var scaryhead = draw.bitmap('img/scaryhead_small.png');
-        scaryhead.x = -29
-        scaryhead.y = -29;
-        enemy.addChild(scaryhead);
-        enemy.x = 400;
-        enemy.y = groundY-50;
-        game.addGameItem(enemy);
-        enemy.velocityX = -1;
-        enemy.rotationalVelocity = 1;
-
-        enemy.onPlayerCollision = function() {
-            console.log('the enemy has hit Halle')
-        };
+        
+        
+        
+        levelData.gameItems.forEach(createGameItem);
 
 
     }
