ca665a2c499bdd2f044843b0fbf1a4c9376caa0f
Harvey Sanders <sanders.harvey@gmail.com>
2015-07-24 10:23:40 -0500

    change species properties to name

--- projects/matchy/part1.js
+++ projects/matchy/part1.js
@@ -1,7 +1,7 @@
 // Use this file to implement Part One of your project
 
 var animal = {};
-animal.species = "Great White Shark";
+animal.name = "Great White Shark";
 animal['tagline'] = "Don't swim with the sharks!";
 animal.noises = null;
 
@@ -17,29 +17,29 @@ animal[noises] = noiseArray;
 var animals = [];
 animals.push(animal);
 
-var quackers = { species: 'duck', tagline: 'Afflack', noises: ['quack', 'honk', 'sneeze', 'growl'] };
+var quackers = { name: 'duck', tagline: 'Afflack', noises: ['quack', 'honk', 'sneeze', 'growl'] };
 animals[1] = quackers;
 
-var polloLoco = { species: 'chicken', tagline: 'finger lickin\' good', noises: ['bok bok', 'cluck cluck', 'bwok bwok', 'smk smk'] };
+var polloLoco = { name: 'chicken', tagline: 'finger lickin\' good', noises: ['bok bok', 'cluck cluck', 'bwok bwok', 'smk smk'] };
 animals.unshift(polloLoco);
 
-var winAmpMascot = { species: 'llama', tagline: 'it really whips the llama\'s ass!', noises: ['spit!', 'hee haw', 'ahroooga', 'pfff'] };
+var winAmpMascot = { name: 'llama', tagline: 'it really whips the llama\'s ass!', noises: ['spit!', 'hee haw', 'ahroooga', 'pfff'] };
 animals.push(winAmpMascot);
 
 var tasmanianDevil = {
-    species: 'sarcophilus harrisii',
+    name: 'sarcophilus harrisii',
     tagline: 'bad a$$ ',
     noises: ['', '', '', ''] };
 
 var friends = [];
-friends.push(polloLoco.species, winAmpMascot.species);
+friends.push(polloLoco.name, winAmpMascot.name);
 
 var relationships = {};
 relationships.friends = friends;
 
 var matches = [];
 relationships.matches = matches;
-relationships.matches.push(quackers.species, animal.species);
+relationships.matches.push(quackers.name, animal.name);
 
 for(var i=0; i<animals.length; i++) {
 	//add relationships to each animal in animals with key of relationships
--- projects/matchy/part2.js
+++ projects/matchy/part2.js
@@ -44,17 +44,17 @@ var deleteFriend = function(friend){
 
 for (var i=0; i<animals.length; i++) {
 	//log i.e.'1. Dog: Who Let the Dogs out?''
-	console.log((i+1) + '. ' + animals[i].species + ': ' + animals[i].tagline);
+	console.log((i+1) + '. ' + animals[i].name + ': ' + animals[i].tagline);
 }
 
 //2b ------------ filter out friends of signed-in (first) animal 
 
 //not friends callback function
 function isFriend(user) {
-	//loop through relationships.friends array and check if matches species from animals object
+	//loop through relationships.friends array and check if matches name from animals object
 	var friends = user.relationships.friends;
 	for(i=0; i<friends.length; i++){
-		if (friends[i] === animals.species){ //gotta loop through animals somehow)
+		if (friends[i] === animals.name){ //gotta loop through animals somehow)
 			return true;
 		} 
 	}
@@ -62,11 +62,11 @@ function isFriend(user) {
 
 // Scenario 3 ----------------------------------------
 
-var findSpecies = function(species) {
+var findAnimal = function(name) {
 	for(i=0; i<animals.length; i++) {
 		for (var key in animals[i]) {
-			if (species === animals[i][key]){
-				console.log('species found!');
+			if (name === animals[i][key]){
+				console.log('name found!');
 				console.log(logPersonalData(animals[i]));
 			}
 		}
@@ -77,8 +77,8 @@ var findSpecies = function(species) {
 
 //Scenario 4 -------------------------------------
 var loggedInUser = animals[0];
-var changeSpecies = function(user, species) {
-	user.species = species;
+var changeName = function(user, name) {
+	user.name = name;
 };
 
 var changeNoises = function(user, noises) {
