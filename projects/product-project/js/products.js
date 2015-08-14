491e05af78b4b21ee3a95a2f6eaccf72549e7e48
Harvey Sanders <sanders.harvey@gmail.com>
2015-08-13 21:45:45 -0500

    add productPrinter, input box, and  basic filter function

---
+++ projects/product-project/js/products.js
@@ -0,0 +1,58 @@
+$(function(){
+	
+	$('#container').attr({
+		'margin' : '10px',
+		'padding' : '10px',
+	});
+
+
+	function printProducts(products){
+		var productsToShow = [];
+
+		$.map(products, function(product, index) {
+			var $productDiv = $('<div>').attr({
+				'class' : 'phone text-left row',
+			});
+			var $productImgDiv = $('<div>').attr({
+				'class':'phone-photo',
+				'class' : 'col-sm-2 text-center',
+			});
+			$productImgDiv.append($('<img>').attr({
+				'src' : 'img/product/thumbs/' + product.image,
+				'height' : '100px',
+			}));
+			var $productInfo = $('<div>').attr({
+				'class' : 'col-sm-10',
+			});
+			var $desc = $('<h4>').text(product.desc);
+			var $price = $('<h6>').text('$' + product.price).css({
+				'color' : 'rgb(33,33,200)',
+			});
+			var $qty = $('<h6>').text('Qty in stock: ' + product.stock);
+
+			productsToShow.push($productDiv.append($productImgDiv, $desc, $price, $qty));
+			
+		});
+		$('.products').html(productsToShow);
+	}
+
+	function searchHandler(query, products){
+		var results = products.filter(function(product){
+			return query.toLowerCase() === product.type.toLowerCase();
+		});
+		return results;
+	}
+
+	$.getJSON('data/product.json', function(products){
+		printProducts(products);
+
+		$('#searchInput').on('input', function(){
+			var query = $('#searchInput').val();
+			printProducts(searchHandler(query, products));
+		});
+
+	});
+
+	
+	
+});
